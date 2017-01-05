989cfd93c4231746994fb1c096b038a09c2dcc90
sreedharbabuk <babjibabu.k@gmail.com>
2017-01-05 17:36:27 +0530

    Initial Checkin to Git Repository

---
+++ DemoMavenGit/src/main/java/org/sree/javabrains/DemoMavenGit/App.java
@@ -0,0 +1,13 @@
+package org.sree.javabrains.DemoMavenGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
